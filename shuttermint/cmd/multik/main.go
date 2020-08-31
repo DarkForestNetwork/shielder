e06a724e97f6b1cb4a794753bf56682989ea2a4e
DFN Admin <darkforest.network@local>
2020-08-31 22:10:43 +0200

    Move the keyper command to cmd/keyper/

---
+++ shuttermint/cmd/multik/main.go
@@ -0,0 +1,24 @@
+// Run multiple keypers in a single process for testing purposes
+package main
+
+import (
+	"log"
+
+	"shielder/shuttermint/keyper"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
+	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
+	if err != nil {
+		panic(err)
+	}
+	err = keyper.Keyper{
+		SigningKey:     privateKey,
+		ShielderURL: "http://localhost:26657",
+	}.Run()
+	if err != nil {
+		panic(err)
+	}
+}
