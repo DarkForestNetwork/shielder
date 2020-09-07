ef1913414bcfaac93c3acdd1dc18c00cfd6f10d6
DFN Admin <darkforest.network@local>
2020-09-07 15:25:24 +0200

    Use cobra for option parsing
    Also combine the shuttermint and keyper programs into a single binary.
    
    `shuttermint run` runs the shutter tendermint app, `shutter keyper`
    runs a keyper process.

---
+++ shuttermint/cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var version string = "(unknown)"
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "shuttermint",
+	Short: "shuttermint runs the shielder tendermint app or a shielder keyper",
+	Long:  `This should become a long description of what shielder is`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
