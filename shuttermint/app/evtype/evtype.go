77f91ae147eee628bc3743f6a14a304875f4746a
DFN Admin <darkforest.network@local>
2020-12-16 14:56:32 +0100

    Add Apology and Accusation to shutterevents
    Also try to cleanup stuff a bit.

--- shuttermint/app/evtype/evtype.go
+++ shuttermint/app/evtype/evtype.go
@@ -2,12 +2,12 @@
 package evtype
 
 var (
-	CheckIn             = "shielder.check-in"
-	PolyCommitment      = "shielder.poly-commitment-registered"
-	PolyEval            = "shielder.poly-eval-registered"
-	BatchConfig         = "shielder.batch-config"
-	DecryptionSignature = "shielder.decryption-signature"
-	EonStarted          = "shielder.eon-started"
 	Accusation          = "shielder.accusation-registered"
 	Apology             = "shielder.apology-registered"
+	BatchConfig         = "shielder.batch-config"
+	CheckIn             = "shielder.check-in"
+	DecryptionSignature = "shielder.decryption-signature"
+	EonStarted          = "shielder.eon-started"
+	PolyCommitment      = "shielder.poly-commitment-registered"
+	PolyEval            = "shielder.poly-eval-registered"
 )
